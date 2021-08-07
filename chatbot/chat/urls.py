52a3188ba2661143e28e9c1603659dc293e9e63c
Sarthak Kumar <kumar.v.sarthak@gmail.com>
2021-08-08 04:19:17 +0530

    formatted code

--- chatbot/chat/urls.py
+++ chatbot/chat/urls.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import logout
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("bot/", views.chat, name="chat"),
     path("login/", views.Flogin, name="login"),
     path("signup/", views.signup, name="signup"),
     path("stat/", views.stat, name="stat"),
     path("logout/", views.Flogout, name="logout"),
-]   
+]
